370c6ebd3a37aae00457721db035f3a75bf2aa03
Sim Shad <samsudeenashad@icloud.com>
2024-11-29 22:32:16 +0530

    update
    update

--- myworld/sim_app/members/views.py
+++ myworld/sim_app/members/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+
 
 from django.shortcuts import render
+
+# Create your views here.
+
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Hello world!")
-# Create your views here.
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())

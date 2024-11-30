4b9cb67ba3022a6ff793d20e8955d0b5543aa370
Sim Shad <samsudeenashad@icloud.com>
2024-11-30 13:04:47 +0530

    update
    update

--- myworld/sim_app/members/views.py
+++ myworld/sim_app/members/views.py
@@ -6,7 +6,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
   template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  rendered_template = template.render()
+  
+  mymembers = Member.object.all().values()  
+  template1 = loader.get_template('all_members.html')
+  context1 = {
+      'mymembers' : mymembers,
+  }
+  
+  rendered_template1 = template1.render(context1, request)
+
+  
+  
+  return HttpResponse(rendered_template)

443a12c5301f8d8f7df51740843bede5e1d16661
Sim Shad <samsudeenashad@icloud.com>
2024-12-01 11:43:05 +0530

    upadte
    upadet

--- myworld/sim_app/members/views.py
+++ myworld/sim_app/members/views.py
@@ -8,18 +8,15 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Member
 
+#def members(request):
+ # template = loader.get_template('myfirst.html')
+  #return HttpResponse(template.render())
+  
+  
 def members(request):
-  template = loader.get_template('myfirst.html')
-  rendered_template = template.render()
-  
-  mymembers = Member.object.all().values()  
-  template1 = loader.get_template('all_members.html')
-  context1 = {
-      'mymembers' : mymembers,
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
   }
-  
-  rendered_template1 = template1.render(context1, request)
-
-  
-  
-  return HttpResponse(rendered_template)
+  return HttpResponse(template.render(context, request))
--- myworld/sim_app/sim_app/urls.py
+++ myworld/sim_app/sim_app/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('members.urls')),
+    
+    
     path('admin/', admin.site.urls),
 ]
